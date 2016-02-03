c71d9224b0627aa73e7394ddc93ccba50d7eba71
dugooder <dugooder@gmail.com>
2016-02-02 21:36:02 -0500

    add views, fixed bugs in build (tests), used html5boiler plate for views base

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,134 @@
-var gulp = require('gulp'),
-    msbuild = require('gulp-msbuild'),
-    del = require('del'),
-    args = require('yargs').argv,
-    assemblyInfo = require('gulp-dotnet-assembly-info'),
-    shell = require('gulp-shell'),
-    fs = require('fs'),
-    request = require('request'),
-    xunit = require('gulp-xunit-runner'),
-    flatten = require('gulp-flatten'),
-    runseq = require('gulp-run-sequence'),
-    colors = require('colors/safe');
+var gulp = require("gulp"),
+    msbuild = require("gulp-msbuild"),
+    del = require("del"),
+    args = require("yargs").argv,
+    assemblyInfo = require("gulp-dotnet-assembly-info"),
+    shell = require("gulp-shell"),
+    fs = require("fs"),
+    request = require("request"),
+    xunit = require("gulp-xunit-runner"),
+    flatten = require("gulp-flatten"),
+    runseq = require("gulp-run-sequence"),
+    colors = require("colors/safe");
 
-var appPackage = require('./package.json');
+var projects = ["common", "service", "service.health", "service.home"];
+var projectsWithTests = ["common", "service.health", "service.home"];
 
-var buildFolder = 'build/';
-var configuration = args.configuration || 'Debug';
+var  pkg = require("./package.json"),
+    srcDir = "src/",
+    buildFolder = "build/",
+    testsSuffix = ".tests",
+    configuration = args.configuration || "Release";
 
-gulp.task('nuget-download', function (done) {
-    if (!fs.existsSync('nuget.exe')) {
-        console.log('Downloading nuget.exe');
-        return request('https://www.nuget.org/nuget.exe').pipe(fs.createWriteStream('nuget.exe'));
+gulp.task("nuget-download", function (done) {
+    if (!fs.existsSync("nuget.exe")) {
+        console.log("Downloading nuget.exe");
+        return request("https://www.nuget.org/nuget.exe").pipe(fs.createWriteStream("nuget.exe"));
     } else {
         console.log("Nuget.exe already exists.");
         return done();
     }
 });
 
-gulp.task('nuget-restore', ['nuget-download'], shell.task([
-    'nuget restore src/' + appPackage.name + '.sln ']
+gulp.task("nuget-restore", ["nuget-download"], shell.task([
+    "nuget restore src/" + pkg.name + ".sln "]
     ));
 
-gulp.task('info', function () {
-    console.log('Building version:' + appPackage.version);
-    console.log('Configuration:' + configuration);
+gulp.task("info", function () {
+    console.log("Building version:" + pkg.version);
+    console.log("Configuration:" + configuration);
 });
 
-gulp.task('assemblyInfo', function () {
+gulp.task("assemblyInfo", function () {
     return gulp
-        .src('**/AssemblyInfo.cs')
+        .src("**/AssemblyInfo.cs")
         .pipe(assemblyInfo({
-            title: appPackage.title,
-            description: appPackage.description,
+            title: pkg.title,
+            description: pkg.description,
             configuration: configuration,
-            company: appPackage.company,
-            product: appPackage.product,
-            trademark: appPackage.trademark,
-            culture: appPackage.culture,
-            version: appPackage.version,
-            fileVersion: appPackage.version,
+            company: pkg.company,
+            product: pkg.product,
+            trademark: pkg.trademark,
+            culture: pkg.culture,
+            version: pkg.version,
+            fileVersion: pkg.version,
             copyright: function () {
-                return appPackage.company + ' ' + new Date().getFullYear();
+                return pkg.company + " " + new Date().getFullYear();
             },
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest("."));
 });
 
-gulp.task('clean', function () {
+gulp.task("clean", function () {
     return del([
-        'src/**/bin/**/*', 
-        'src/**/obj/**/*', 
-        buildFolder + '*'], {force: true});
+        "src/**/bin/**/*",
+        "src/**/obj/**/*",
+        buildFolder + "*"], { force: true });
 });
 
-gulp.task('compile', function () {
-    if (!fs.existsSync(buildFolder)){
+gulp.task("compile", function () {
+    if (!fs.existsSync(buildFolder)) {
         fs.mkdirSync(buildFolder);
     }
 
     return gulp
-        .src('**/*.sln')
+        .src("**/" + pkg.name + ".sln")
         .pipe(msbuild({
             toolsVersion: 14.0,
-            targets: ['Rebuild'],
+            targets: ["Rebuild"],
             errorOnFail: true,
             stdout: true,
-            fileLoggerParameters: 'LogFile=' + buildFolder +  'Build.log;Verbosity=minimal',
+            fileLoggerParameters: "LogFile=" + buildFolder + "Build.log;Verbosity=Normal",
             properties: { Configuration: configuration }
         }));
 });
 
-gulp.task('test', ['compile'], function () {
-   return gulp.src(['**/bin/**/*.tests.dll'], { read: false })
-       .pipe(xunit({
-           executable: 'src/packages/xunit.runner.console.2.1.0/tools/xunit.console.exe',
-           options: {
-               nologo: true,
-               html: buildFolder + 'test_results.html'
-           }
-       }));
+gulp.task("test", ["compile"], function () {
+    var testsDllDir = [];
+
+    for (var i = 0; i < projectsWithTests.length; i++) {
+        var p = projectsWithTests[i];
+        testsDllDir.push(srcDir + p + testsSuffix +
+            "/bin/" + configuration + "/*" +
+            p + testsSuffix + ".dll");
+    }
+
+    console.log(testsDllDir);
+
+    return gulp.src(testsDllDir, { read: false })
+        .pipe(xunit({
+            executable: "src/packages/xunit.runner.console.2.1.0/tools/xunit.console.exe",
+            options: {
+                nologo: true,
+                html: buildFolder + "test_results.html"
+            }
+        }));
 });
 
-gulp.task('package', function () {
-    var baseBinMatch = 'src/**/bin/' + configuration + '/'; 
-    return gulp.src([
-            baseBinMatch + '*.{exe,dll,config}',
-            '!' + baseBinMatch + '*.tests.*',
-            '!' + baseBinMatch + '*xunit.*',
-            ])
-        .pipe(flatten())
+gulp.task("package", function () {
+     var projectsToPack = [];
+
+    for (var i = 0; i < projects.length; i++) {
+        var p = projects[i];
+        projectsToPack.push(srcDir + p +
+            "/bin/" + configuration +
+            '/**/*.*');
+    }
+
+    console.log(projectsToPack);
+      
+    return gulp.src(projectsToPack)
         .pipe(gulp.dest(buildFolder));
 });
 
-gulp.task('build', function(callback) {
-        runseq('info',
-              'clean',
-              'nuget-restore',
-              'assemblyInfo',
-              'compile',
-              'test',
-              'package',
-              'info',
-              callback);
+gulp.task("build", function (callback) {
+    runseq("info",
+        "clean",
+        "nuget-restore",
+        "assemblyInfo",
+        "compile",
+        "test",
+        "package",
+        "info",
+        callback);
 });
